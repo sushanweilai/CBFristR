4ba5e46c2d9fbcc7db2465e0551b3e028cd7311b
sushanweilai <2025444919@qq.com>
2018-03-07 21:50:30 +0800

    mark

---
+++ webserver/1.py
@@ -0,0 +1,29 @@
+import BaseHTTPServer
+
+class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    '''监听　http　请求　返回／page'''
+    #page to send back
+    Page = '''\
+<html>
+<body>
+<p>hello ,world </p>
+</body>
+</html>
+'''
+    #监听一个ＧＥＴ请求
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type","text/html")
+        self.send_header("Content-Length",str(len(self.Page)))
+        self.end_headers()
+        self.wfile.write(self.Page)
+
+
+
+
+
+if __name__=='__main__':
+    serverAddress = ('',8080)
+    server = BaseHTTPServer.HTTPServer(serverAddress,RequestHandler)
+    server.server_forever()
+
